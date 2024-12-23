9efe1db1640cf746cf17642652901a398453c405
Paola Balderas <pao.315@gmail.com>
2024-12-23 10:26:29 -0600

    Push commit

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function RangeAlert() {
     password += CheckedSets[Math.floor(Math.random() * CheckedSets.length)].characters[Math.floor(Math.random() * CheckedSets[0].characters.length)];
 
     while (password.length < PasswordLength) {
-        const RandomCharacters = CheckedSets[Math.floor(Math.random() * CheckedSets.length)];
+        const RandomCharacters =
+            CheckedSets[Math.floor(Math.random() * CheckedSets.length)];
         password += RandomCharacters.characters[Math.floor(Math.random() * RandomCharacters.characters.length)];
     }
 
